7b3b381b0f7e628302a27b24f3857c44e8726574
Iryna Vakilova <iravakilova@gmail.com>
2022-06-20 23:14:04 +0300

    task-1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = galleryItems
+    .map(({ preview, original, description }) =>
+        `<a class = 'gallery__item' href = '${original}'>
+            <img class = 'gallery__image'
+            src = ${preview}
+            alt = ${description}
+            />
+        </a>`)
+    .join(''); 
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+gallery.addEventListener('click', clickImage);
+function clickImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        event,
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionDelay: 250,
+        });
+};
