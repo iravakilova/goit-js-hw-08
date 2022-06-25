8f00f0a1208a38572c9cd27c95c267ef1f7a3cdb
Iryna Vakilova <iravakilova@gmail.com>
2022-06-25 23:27:42 +0300

    task-3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name = "email"]');
+const message = document.querySelector('[name = "message"]');
+const KEY = "feedback-form-state";
+
+form.addEventListener("input", throttle(onFormInput, 500));
+function onFormInput() {
+    const user = {
+        email: email.value,
+        message: message.value,
+    };
+    localStorage.setItem(KEY, JSON.stringify(user));
+}
+
+loadPage();
+function loadPage() {
+    const userParse = JSON.parse(localStorage.getItem(KEY));
+    if (userParse) {
+        email.value = userParse.email;
+        message.value = userParse.message;
+    }
+    email.value = '';
+    message.value = '';
+}
+
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(KEY)));
+    form.reset();
+    localStorage.clear();
+}
